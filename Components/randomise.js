36467f48a90539e850b351b4b2f05d4d6907cef4
neetas <neeta.sarl@7digital.com>
2016-04-30 22:58:23 +0100

    First commit

---
+++ Components/randomise.js
@@ -0,0 +1,26 @@
+$(document).ready(jaksbars);
+
+function jaksbars() {
+$( "#bars" ).submit(randomisebars)
+}
+function randomisebars( event ) {
+
+  var $form = $(this),
+    inputrhymes = $form.find("input[name='inputrhymes']").val();
+    var verse = inputrhymes.split(";");
+    var randomise = verse[Math.floor(Math.random() * verse.length)];
+    var splitline = randomise.split("_");
+    $('line1').text(splitline[0]);
+    $('line2').text(splitline[1]);
+    event.preventDefault();
+}
+function preventEnterSubmission()
+{
+  $('#bars').keypress(function(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      this.value = this.value + "\n";
+    }
+  });
+}
+setInterval(function(){ "jaksbars()" }, 1000);
