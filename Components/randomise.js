8546f8621d7bb239752f9467572690afa28c1a76
neetas <neeta.sarl@7digital.com>
2016-05-02 19:19:01 +0100

    refactor split line

--- Components/randomise.js
+++ Components/randomise.js
@@ -2,7 +2,7 @@ $(document).ready(jaksbars);
 
 function jaksbars() {
   $( "#bars" ).submit(randomisebars);
-  setInterval(function(){ $("#bars").submit() }, 1000);
+  setInterval(function(){ $("#bars").submit() }, 3000);
 }
 
 function randomisebars( event ) {
@@ -11,8 +11,6 @@ function randomisebars( event ) {
     inputrhymes = $form.find("textarea[name='inputrhymes']").val();
     var verse = inputrhymes.split(";");
     var randomise = verse[Math.floor(Math.random() * verse.length)];
-    var splitline = randomise.split("_");
-    $('line1').text(splitline[0]);
-    $('line2').text(splitline[1]);
+    $('randomverse').text(randomise);
     event.preventDefault();
 }
