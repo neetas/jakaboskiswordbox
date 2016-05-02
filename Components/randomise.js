eb8821d32979db7a3e4e512d24c95832c38338e1
Alex Graff <agraff@users.noreply.github.com>
2016-05-02 16:14:47 +0100

    Multiline text box

--- Components/randomise.js
+++ Components/randomise.js
@@ -8,20 +8,11 @@ function jaksbars() {
 function randomisebars( event ) {
 
   var $form = $(this),
-    inputrhymes = $form.find("input[name='inputrhymes']").val();
+    inputrhymes = $form.find("textarea[name='inputrhymes']").val();
     var verse = inputrhymes.split(";");
     var randomise = verse[Math.floor(Math.random() * verse.length)];
     var splitline = randomise.split("_");
     $('line1').text(splitline[0]);
     $('line2').text(splitline[1]);
     event.preventDefault();
-}
-function preventEnterSubmission()
-{
-  $('#bars').keypress(function(event) {
-    if (event.which == 13) {
-      event.preventDefault();
-      this.value = this.value + "\n";
-    }
-  });
 }
