8ef097a271b5cebed03f45cfa60db75888c68fe6
Neeta <neeta.sarl@7digital.com>
2018-02-06 15:14:52 +0000

    Split Line with underscores

--- Components/randomise.js
+++ Components/randomise.js
@@ -11,6 +11,9 @@ function randomisebars( event ) {
     inputrhymes = $form.find("textarea[name='inputrhymes']").val();
     var verse = inputrhymes.split(";");
     var randomise = verse[Math.floor(Math.random() * verse.length)];
+    var splitline = randomise.split("_");
+    $('line1').text(splitline[0]);
+    $('line2').text(splitline[1]);
     $('randomverse').text(randomise);
     event.preventDefault();
 }
