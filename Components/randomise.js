384d15920cc73b3ee18177ee1a29235945112265
Neeta <neeta.sarl@7digital.com>
2018-02-06 15:30:55 +0000

    split verse lines

--- Components/randomise.js
+++ Components/randomise.js
@@ -1,14 +1,13 @@
 $(document).ready(jaksbars);
 
 function jaksbars() {
-  $( "#bars" ).submit(randomisebars);
+$( "#bars" ).submit(randomisebars)
   setInterval(function(){ $("#bars").submit() }, 3000);
 }
-
 function randomisebars( event ) {
 
   var $form = $(this),
-    inputrhymes = $form.find("textarea[name='inputrhymes']").val();
+    inputrhymes = $form.find("input[name='inputrhymes']").val();
     var verse = inputrhymes.split(";");
     var randomise = verse[Math.floor(Math.random() * verse.length)];
     var splitline = randomise.split("_");
@@ -17,3 +16,4 @@ function randomisebars( event ) {
     $('randomverse').text(randomise);
     event.preventDefault();
 }
+
